06f4b3a1de14240ba36d181bfff1e165813ef87a
mdmasrafimahbubnirob <mdmasrafimahbubnirob@gmail.com>
2026-05-12 20:51:47 +0600

    test again

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,26 @@ function App() {
 
       {/* <div className='mx-auto'> */}
 
-        <div className="mx-auto card bg-primary text-primary-content w-96">
-          <div className="card-body">
-            <h2 className="card-title">Card title!</h2>
-            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-            <div className="card-actions justify-end">
-              <button className="btn">Buy Now</button>
-            </div>
+      <div className="mx-auto card bg-primary text-primary-content w-96">
+        <div className="card-body">
+          <h2 className="card-title">Card title!</h2>
+          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+          <div className="card-actions justify-end">
+            <button className="btn">Buy Now</button>
           </div>
         </div>
+      </div>
       {/* </div> */}
+
+      <div className='mt-5 mx-auto w-80 border border-red-600 bg-yellow-200 shadow-2xl shadow-red-600 text-shadow-transparent rounded-2xl'>
+        <div className="card-body">
+          <h2 className="card-title">Card title!</h2>
+          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+          <div className="card-actions justify-end">
+            <button className="btn">Buy Now</button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
