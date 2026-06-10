6ffcae5f82968d47b955da1efe34e64a1bb3c22b
mdmasrafimahbubnirob <mdmasrafimahbubnirob@gmail.com>
2026-06-10 09:05:45 +0600

    test 2

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,12 @@ function App() {
 
   return (
     <>
-      <h1 className='text-red-500 text-3xl'>Hi, My name is Masrafi .i love u</h1>
-      <p className='btn bg-amber-400 hover:bg-amber-600 text-blue-400'>Click Me</p>
-      <div className='cart'>hi</div>
 
-      {/* <div className='mx-auto'> */}
-
-      <div className="mx-auto card bg-primary text-primary-content w-96">
-        <div className="card-body">
-          <h2 className="card-title">Card title!</h2>
-          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-          <div className="card-actions justify-end">
-            <button className="btn">Buy Now</button>
-          </div>
-        </div>
+      <div className='mx-auto border border-yellow-500 min-w-3xl max-w-5xl mt-3 bg-amber-400'>
+        <h1 className='text-6xl mx-auto text-fuchsia-500 text-center'>I Love You</h1>
       </div>
-      {/* </div> */}
 
-      <div className='mt-5 mx-auto w-80 border border-red-600 bg-yellow-200 shadow-2xl shadow-red-600 text-shadow-transparent rounded-2xl'>
-        <div className="card-body">
-          <h2 className="card-title">Card title!</h2>
-          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-          <div className="card-actions justify-end">
-            <button className="btn">Buy Now</button>
-          </div>
-        </div>
-      </div>
+
     </>
   )
 }
