447a97e1794fbb19ed491a5d9fb93d6eb05370ba
mdmasrafimahbubnirob <mdmasrafimahbubnirob@gmail.com>
2026-06-20 22:13:22 +0600

    working on navbar - mobile responsive solved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
         <div className="">
 
 
-            <div className="navbar bg-base-100 shadow-sm px-[8%]">
+            <div className="navbar bg-base-100 shadow-sm lg:px-[8%]">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
