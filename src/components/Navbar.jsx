2047bdd6e12df64e38e99a8599c2afd70024cefc
mdmasrafimahbubnirob <mdmasrafimahbubnirob@gmail.com>
2026-06-20 23:45:18 +0600

    navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,14 @@ const Navbar = () => {
 
 
 
-                <div className="navbar-end">
+                <div className="navbar-end space-x-4">
+
+                    <div className="indicator">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
+                        <span className="border-red-400 badge badge-sm indicator-item">8</span>
+                    </div>
+
+
                     <a className="btn rounded-full text-amber-50 bg-linear-to-r from-[#4f39f6] via-[#7226f8] to-[#9514fa] hover:text-red-300">Get Started</a>
                 </div>
             </div>
